8877910017613f3b6560ec5795f2714379eac9a6
Glenn <islandcreation@outlook.com>
2023-08-28 14:22:45 -0400

    Update problem_016.py

--- problem_016.py
+++ problem_016.py
@@ -1,27 +1,6 @@
-<<<<<<< HEAD
 # Complete the is_inside_bounds function which takes an x
 # coordinate and a y coordinate, and then tests each to
 # make sure they're between 0 and 10, inclusive.
 
 def is_inside_bounds(x, y):
-    if x >= 0 and y >= 0 and x <= 10 and y <= 10:
-        return True
-    else:
-        return False
-
-print(is_inside_bounds(1,2))
-print(is_inside_bounds(-1,11))
-=======
-# Complete the is_inside_bounds function which takes an x
-# coordinate and a y coordinate, and then tests each to
-# make sure they're between 0 and 10, inclusive.
-
-def is_inside_bounds(x, y):
-    if x >= 0 and y >= 0 and x <= 10 and y <= 10:
-        return True
-    else:
-        return False
-
-print(is_inside_bounds(1,2))
-print(is_inside_bounds(-1,11))
->>>>>>> 6890c8f5fa8daba58f58887c821d440a15b9ee8d
+    pass
